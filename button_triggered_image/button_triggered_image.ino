51b1e0d82508123cd28505716adf9066896ed0f1
nicolet2025 <168128525+nicolet2025@users.noreply.github.com>
2025-02-22 15:17:07 -0600

    Update button_triggered_image.ino

--- button_triggered_image/button_triggered_image.ino
+++ button_triggered_image/button_triggered_image.ino
@@ -76,7 +76,7 @@ void setup() {
   // camera init
   esp_err_t err = esp_camera_init(&config);
   if (err != ESP_OK) {
-    Serial.printfln("Camera init failed with error 0x%x", err);
+    Serial.printf("Camera init failed with error 0x%x\n", err);
     return;
   }
   
